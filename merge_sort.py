86424882ad89f49d7b01ffebf0b4371ec06b05a7
lion137 <lion137@gmail.com>
2017-01-19 13:59:36 +0000

    Add files via upload

---
+++ merge_sort.py
@@ -0,0 +1,40 @@
+# merge sort
+
+
+def merge_sort(alist):
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        merge_sort(lefthalf)
+        merge_sort(righthalf)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i += 1
+            else:
+                alist[k] = righthalf[j]
+                j += 1
+            k += 1
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i += 1
+            k += 1
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j += 1
+            k += 1
+
+
+alist1 = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+merge_sort(alist1)
+print(alist1)
+
+'''
+output -> [17, 20, 26, 31, 44, 54, 55, 77, 93]
+'''
