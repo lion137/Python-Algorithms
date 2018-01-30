928ec58e839972b1d4c4f47b2650542792b783e1
lion137 <lion137@gmail.com>
2018-01-30 12:41:52 +0000

    factoroization fixed

--- factorization_to_primes.py
+++ factorization_to_primes.py
@@ -6,11 +6,7 @@
 
 # Function factorize_trial
 
-from Euler import is_prime, prime_sieve, segmented_sieve, modularexponenation, miller_rabin2, miller_rabin
-from math import log, e
-import time
-import sympy
-
+from Euler import miller_rabin
 
 
 def factorize_trial(n, primes):
@@ -28,26 +24,4 @@ def factorize_trial(n, primes):
     return result
 
 
-def primes_array(k, n, pr_array):
-    if k % 2 == 0: # make k odd
-        k += 1
-    out = []
-    """returns primes from k to n (trial algorithm)"""
-    for i in range(k + 2, n + 1 , 2):
-        if miller_rabin(i):
-           out.append(i)
-    return pr_array + out
-
-pr = 200000000
-primes = prime_sieve(pr)
-pr_len = pr // log(pr, e)
-print(pr_len, pr_len ** 2)
-n = 5123441 * 997
-print(len(primes), len((primes)) ** 2)
-st = time.time()
-for x in range(10000):
-    factors = factorize_trial(n, primes)
-end = time.time()
-print(f'Time: {end - st:.2f}s\n')
-print("factors", factors)
 
